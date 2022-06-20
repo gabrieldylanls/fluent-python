7b404094d1314ddf4b7479d2faab3ec4b71bd496
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2022-06-20 12:58:38 -0300

    Assigning values to ranks

--- data_model/card_deck.py
+++ data_model/card_deck.py
@@ -1,6 +1,7 @@
 import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
@@ -14,3 +15,7 @@ class FrenchDeck:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
