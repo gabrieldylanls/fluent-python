3047eb4d6bcf455aeb57b42c8b247b70d72873f2
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2022-06-23 20:05:14 -0300

    Cartesian product in a generator expression.

--- sequences/genexps.py
+++ sequences/genexps.py
@@ -5,3 +5,10 @@ symbols = '$%#&*'
 print(tuple(ord(s) for s in symbols))
 
 print(array.array('I', (ord(s) for s in symbols)))
+
+#Example 2-6. Cartesian product in a generator expression.
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+
+for tshirt in (f'{c} {s}' for c in colors for s in sizes):
+    print(tshirt)
