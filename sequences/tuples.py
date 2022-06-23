bdafa7d83471fc42c40572cb133093d8748cc601
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2022-06-23 20:57:09 -0300

    Examples of tuple unpacking

--- sequences/tuples.py
+++ sequences/tuples.py
@@ -8,3 +8,38 @@ for passport in sorted(traveler_ids):
 
 for country, _ in traveler_ids:
     print(country)
+
+#Examples of tuple unpacking
+latitude, longitude = lax_coordinates
+print('Latitude', latitude)
+print('Longitude', longitude)
+
+a = 1
+b = 2
+
+b, a = a, b
+
+print('a', a)
+print('b', b)
+
+t = (20, 8)
+print(divmod(*t))
+
+quotient, remainder = divmod(*t)
+print('Quotient', quotient)
+print('Remainder', remainder)
+
+import os
+_, filename = os.path.split('/home/luciano/.ssh/idrsa.pub')
+print('Filename', filename)
+
+a, b, *rest = range(5)
+print((a, b, rest))
+a, b, *rest = range(3)
+print((a, b, rest))
+a, b, *rest = range(2)
+print((a, b, rest))
+a, *body, c, d = range(5)
+print((a, body, c, d))
+*head, b, c, d = range(5)
+print((head, b, c, d))
