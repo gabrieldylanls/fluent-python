e36f3e9df963b467645ef922e6402da361a7c6d5
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2022-06-23 20:15:02 -0300

    Tuples used as records.

---
+++ sequences/tuples.py
@@ -0,0 +1,10 @@
+#Example 2-7. Tuples used as records.
+lax_coordinates = (33.9425, -118.408056)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'CE342567')]
+
+for passport in sorted(traveler_ids):
+    print(f'{passport[0]}/{passport[1]}')
+
+for country, _ in traveler_ids:
+    print(country)
